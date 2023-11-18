dfe59e33b07a88a1662c2d6f2c3c09bdff1e4869
Breno Andrade <brenottandrade@gmail.com>
2023-11-17 23:25:03 -0300

    Teclas dinâmicas, múltiplas notas ao mesmo tempo.

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,3 +1,30 @@
+function createKeys(value, box, color) {
+    let key = document.createElement("li");
+        key.className = `key ${color}`;
+        key.dataset.key = value;
+        key.innerHTML = `<span>${value}</span>`;
+        box.appendChild(key);
+}
+
+function setKeys () {
+    let box = document.getElementById("key-box");
+    for (let i = 0; i < 2; i++) {
+        createKeys("a", box, "white");
+        createKeys("w", box, "black");
+        createKeys("s", box, "white");
+        createKeys("e", box, "black");
+        createKeys("d", box, "white");
+        createKeys("t", box, "white");
+        createKeys("g", box, "black");
+        createKeys("y", box, "white");
+        createKeys("h", box, "black");
+        createKeys("u", box, "white");
+        createKeys("j", box, "black");
+        createKeys("k", box, "white");           
+    }
+}
+setKeys();
+
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
 const volumeSlider = document.querySelector(".volume-slider input");
@@ -9,25 +36,24 @@ let audio = new Audio("src/sounds/a.wav");
 let mapedKeys = [];
 
 const playTune = (key) => {
-    audio.src = `src/sounds/${key}.wav`
+    let audio = new Audio(`src/sounds/${key}.wav`);
     audio.play();
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
     clickedKey.classList.add("active");
     setTimeout(() => {
         clickedKey.classList.remove("active");
-    }, 150);
+    }, 100);
 };
 
 pianoKeys.forEach((key) => {
-    console.log(key.dataset.key);
-    key.addEventListener("click", () => playTune(key.dataset.key));
     mapedKeys.push(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
 document.addEventListener("keydown", (e) => {
     if (mapedKeys.includes(e.key)) {
-        playTune(e.key);
+      playTune(e.key);
     }
 });
 
